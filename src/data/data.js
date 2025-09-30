534f33085fdd82865bcdcde828e8b3845095e9cd
Guillermo Jimenez <jmg201098@gmail.com>
2025-09-29 19:02:17 -0600

    Skill added and modified

---
+++ src/data/data.js
@@ -0,0 +1,74 @@
+const data = {
+    "skills" : {
+        "technical" : {
+            "bootstrap" : {
+                "icon" : "iconBootstrap",
+                "title" : "Bootstrap Framework",
+                "percentage_experience" : 80,
+                "emphasis_color" : "#7110F4",
+                "mastered_topics" : "Diseños responsivos,Modelo Grid y Flex,UI/UX,Gaps,Diseños intuitivos y fáciles de usar"
+            },
+            "vue" : {
+                "icon" : "iconVue",
+                "title" : "Vue Framework",
+                "percentage_experience" : 60,
+                "emphasis_color" : "#327959",
+                "mastered_topics" : "Diseño por componentes,Manejo de islas,Enrrutamiento,SAP"
+            },
+            "html5" : {
+                "icon" : "iconHTML5",
+                "title" : "HTML5",
+                "percentage_experience" : 100,
+                "emphasis_color" : "#DD4B25",
+                "mastered_topics" : "SEO,Cavas,iFrames,Prácticas en buena estructuración"
+            },
+            "css3" : {
+                "icon" : "iconCSS3",
+                "title" : "CSS3",
+                "percentage_experience" : 80,
+                "emphasis_color" : "#264DE4",
+                "mastered_topics" : "Grid,Flex,Animaciones,Transiciones,Fuentes personalizadas,Iconos,Manejo SVGs"
+            },
+            "nodejs" : {
+                "icon" : "iconNodeJS",
+                "title" : "Node JS",
+                "percentage_experience" : 60,
+                "emphasis_color" : "#3F873B",
+                "mastered_topics" : "Streams y buffers,Operaciones con archivos de sistema,Express.js,Callbacks,Promesas,Async/Await,Operaciones CRUD,Manejadores de DB,Autenticaciones y automatizaciones,API RestFull,Servidores HTTP/HTTPS"
+            },
+            "reactjs" : {
+                "icon" : "iconReactJS",
+                "title" : "React JS",
+                "percentage_experience" : 70,
+                "emphasis_color" : "#1399C4",
+                "mastered_topics" : "Componentes,Hooks,Hooks personalizados,Router"
+            },
+            "javascript" : {
+                "icon" : "iconJavaScript",
+                "title" : "JavaScript",
+                "percentage_experience" : 80,
+                "emphasis_color" : "#EBDA1C",
+                "mastered_topics" : "Callbacks,Async/Await,Funciones flecha,Desestructuración,Modulos,Import,Export,POO,Programación funcional"
+            },
+            "gitlab" : {
+                "icon" : "iconGitLab",
+                "title" : "GitLab",
+                "percentage_experience" : 80,
+                "emphasis_color" : "#E24329",
+                "mastered_topics" : "Callbacks,Async/Await,Funciones flecha,Desestructuración,Modulos,Import,Export,POO,Programación funcional"
+            },
+            "tailwind" : {
+                "icon" : "iconTailwind",
+                "title" : "JavaScript",
+                "percentage_experience" : 80,
+                "emphasis_color" : "#3EBFF8",
+                "mastered_topics" : "Callbacks,Async/Await,Funciones flecha,Desestructuración,Modulos,Import,Export,POO,Programación funcional"
+            }
+        },
+        "soft" : {
+
+        }
+    }
+}
+
+export default data;
